f313343055de42bf8424a81c4d74eb7419300ebb
coderkkq <9740273+coderkkq@user.noreply.gitee.com>
2021-11-13 15:15:54 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button type="info" @click="logOut">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 点击首页退出按钮
+    logOut() {
+      // 清空浏览器本地的token值
+      window.sessionStorage.clear()
+      // 退出跳转到login页面
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {  } from 'element-ui'
 
+// 自定义导入（按需导入）
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+// 全局注册组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// Message组件比较特殊，需要全局挂载，$message（这是自定义的名字）
+Vue.prototype.$message = Message
