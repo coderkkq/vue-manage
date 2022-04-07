1ac3ef02a76262de8975c4ee4dbe6a440834a308
coderkkq <9740273+coderkkq@user.noreply.gitee.com>
2022-04-07 14:15:03 +0800

    finish

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -15,41 +15,41 @@
 <script>
 import * as echarts from "echarts";
 // 导入合并对象的方法
-import {merger} from '../../common/util'
+// import {merger} from '../../common/util'
 export default {
   data() {
     return {
       // 这是需要合并的数据
-      options: {
-        title: {
-          text: "用户来源",
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "cross",
-            label: {
-              backgroundColor: "#E9EEF3",
-            },
-          },
-        },
-        grid: {
-          left: "3%",
-          right: "4%",
-          bottom: "3%",
-          containLabel: true,
-        },
-        xAxis: [
-          {
-            boundaryGap: false,
-          },
-        ],
-        yAxis: [
-          {
-            type: "value",
-          },
-        ],
-      },
+      // options: {
+      //   title: {
+      //     text: "用户来源",
+      //   },
+      //   tooltip: {
+      //     trigger: "axis",
+      //     axisPointer: {
+      //       type: "cross",
+      //       label: {
+      //         backgroundColor: "#E9EEF3",
+      //       },
+      //     },
+      //   },
+      //   grid: {
+      //     left: "3%",
+      //     right: "4%",
+      //     bottom: "3%",
+      //     containLabel: true,
+      //   },
+      //   xAxis: [
+      //     {
+      //       boundaryGap: false,
+      //     },
+      //   ],
+      //   yAxis: [
+      //     {
+      //       type: "value",
+      //     },
+      //   ],
+      // },
     };
   },
   created() {},
@@ -64,7 +64,7 @@ export default {
       return this.$message.error("获取数据失败！");
     }
     // 合并对象
-    res.data = merger(res.data, this.options)
+    // res.data = merger(res.data, this.options)
     console.log(res.data);
     // 获取数据成功后 ，绘制图表
     myChart.setOption(res.data);
